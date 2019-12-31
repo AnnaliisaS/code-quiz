fd902e4998b627925fc1dd43296b3e227964c3ac
Annaliisa Tongpattanakul <annaliisa@Annaliisas-MacBook-Pro.local>
2019-12-30 23:11:41 -1000

    created a quiz with radio buttons aligned vertically

--- questions.js
+++ questions.js
@@ -1,3 +1,88 @@
+const quizContainer = document.getElementById('quiz');
+const myQuestions = [
+    {
+        question: 'JavaScript is what kind of language?',
+        answers: {
+            a: 'Strongly typed',
+            b: 'Loosely typed',
+            c: 'Phonetic',
+            d: 'None of the above'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: 'In order to run a function, you must first: ',
+        answers: {
+            a: 'Name it something memorable',
+            b: 'List at least 2 arguments',
+            c: 'Call the function',
+            d: 'Verbally threaten the computer'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: 'The contents of an array are enclosed within: ',
+        answers: {
+            a: 'Curly brackets',
+            b: 'Parentheses',
+            c: 'Quotes',
+            d: 'Square brackets'
+        },
+        correctAnswer: 'd'
+    },
+    {
+        question: 'Strings in JavaScript must be contained within: ',
+        answers: {
+            a: 'Double quotation marks',
+            b: 'Single quotation marks',
+            c: 'Either single or double quotation marks',
+            d: 'None of the above'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: 'Some common issues students have when learning JavaScript are: ',
+        answers: {
+            a: 'typos',
+            b: 'forgetting semicolons',
+            c: 'forgetting to call a function',
+            d: 'All of the above'
+        },
+        correctAnswer: 'd'
+    },
+];
+
+function buildQuiz(){
+//to store the HTML output
+  const output = [];
+
+  // for each question store the list of answer choices
+  myQuestions.forEach(
+    (currentQuestion, questionNumber) => {
+      const answers = [];
+
+      //for each answer add an HTML radio button
+      for(letter in currentQuestion.answers){
+        answers.push(
+          `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
+        );
+      }
+      // add this question and its answers to the output
+      output.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join('')} </div>`
+      );
+    }
+  );
+
+  //combine output list into one string of HTML and put it on the page
+  quizContainer.innerHTML = output.join('');
+}
+
 let button = document.getElementById('start');
 
 button.addEventListener('click', function () {
@@ -14,31 +99,3 @@ button.addEventListener('click', function () {
         }
     }, 1000);
 })
-
-const questions = [
-    {
-        question: 'JavaScript is what kind of language?',
-        choices: ['Strongly typed', 'Loosely typed', 'Phonetic', 'None of the above'],
-        answer: 'Loosely typed'
-    },
-    {
-        question: 'In order to run a function, you must first: ',
-        choices: ['Name it something memorable', 'List at least 2 arguments', 'Call the function', 'Verbally threaten the computer'],
-        answer: 'Call the function'
-    },
-    {
-        question: 'The contents of an array are enclosed within: ',
-        choices: ['Curly brackets', 'Parentheses', 'Quotes', 'Square brackets'],
-        answer: 'Square brackets'
-    },
-    {
-        question: 'Strings in JavaScript must be contained within: ',
-        choices: ['Double quotation marks', 'Single quotation marks', 'Either single or double quotation marks', 'None of the above'],
-        answer: 'Either single or double quotation marks'
-    },
-    {
-        question: 'Some common issues students have when learning JavaScript are: ',
-        choices: ['typos', 'forgetting semicolons', 'forgetting to call a function', 'All of the above'],
-        answer: 'All of the above'
-    },
-];
